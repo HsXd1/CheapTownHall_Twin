67ac35c508aaeb04d285f30085e7e35b332decf4
BananazrCRAZY <76630376+BananazrCRAZY@users.noreply.github.com>
2025-04-18 20:17:34 -0700

    updated page views and other features

---
+++ SPA/frontend/static/js/index.js
@@ -0,0 +1,44 @@
+const navigateTo = url => {
+    history.pushState(null, null, url);
+    router();
+};
+
+const router = async () => {
+    const routes = [
+        { path: "/", view: () => console.log("viewing dashboard") },
+        { path: "/login", view: () => console.log("viewing login") }
+    ];
+
+    // test each rout for potential match
+    const potentialMatches = routes.map(route => {
+        return {
+            route: route,
+            isMatch: location.pathname === route.path
+        };
+    });
+
+    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+
+    // if page not found, sent back to base page
+    if (!match) {
+        match = {
+            route: routes[0],
+            isMatch: true
+        };
+    }
+
+    console.log(match.route.view());
+};
+
+window.addEventListener("popstate", router);
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("click", e => {
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    });
+
+    router();
+});
--- SPA/server.js
+++ SPA/server.js
@@ -3,8 +3,10 @@ const path = require("path");
 
 const app = express();
 
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+
 app.get("/{*any}", (req, res, next) => {
-    res.sendFile(path.resolve("frontend", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
 
 app.listen(process.env.PORT || 8000, () => console.log("Server running..."));
